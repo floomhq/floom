c18c124371b61854c5e91b919eefaac7cea5be58
vladbakhteev <bakhteev.vladd@gmail.com>
2026-04-04 14:52:25 +0200

    try to fix cors

--- convex/files.ts
+++ convex/files.ts
@@ -15,6 +15,7 @@ export function getR2Client(): S3Client {
       accessKeyId: process.env.R2_ACCESS_KEY_ID!,
       secretAccessKey: process.env.R2_SECRET_ACCESS_KEY!,
     },
+    forcePathStyle: true,
     requestChecksumCalculation: "WHEN_REQUIRED",
     responseChecksumValidation: "WHEN_REQUIRED",
   });
